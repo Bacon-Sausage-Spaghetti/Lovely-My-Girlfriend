598324758fa6265d64dbc06541f7c23fa27d113f
101 <tmddnjs001133@dsm.hs.kr>
2024-05-27 21:44:58 +0900

    first commit

---
+++ src/main/kotlin/com/baconsausagespaghetti/mylovelygirlfriend/MyLovelyGirlfriendApplication.kt
@@ -0,0 +1,11 @@
+package com.baconsausagespaghetti.mylovelygirlfriend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLovelyGirlfriendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyLovelyGirlfriendApplication>(*args)
+}
---
+++ src/test/kotlin/com/baconsausagespaghetti/mylovelygirlfriend/MyLovelyGirlfriendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baconsausagespaghetti.mylovelygirlfriend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLovelyGirlfriendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
