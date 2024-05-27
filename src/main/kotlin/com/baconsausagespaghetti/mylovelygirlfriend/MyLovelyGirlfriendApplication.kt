08f01f37c1f6a5defc90ef9fe7783788a4070764
101 <tmddnjs001133@dsm.hs.kr>
2024-05-27 21:53:58 +0900

    # D + 1

--- src/main/kotlin/com/baconsausagespaghetti/mylovelygirlfriend/MyLovelyGirlfriendApplication.kt
+++ src/main/kotlin/com/baconsausagespaghetti/mylovelygirlfriend/MyLovelyGirlfriendApplication.kt
@@ -2,8 +2,12 @@ package com.baconsausagespaghetti.mylovelygirlfriend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.web.reactive.config.EnableWebFlux
 
+@EnableWebFlux
 @SpringBootApplication
+@EnableWebFluxSecurity
 class MyLovelyGirlfriendApplication
 
 fun main(args: Array<String>) {
